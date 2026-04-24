e4816e2b3fcf582ccae829784a46165b13f572ae
Thiago Allves <101152758+thiallves@users.noreply.github.com>
2026-04-24 17:13:01 -0300

    Update database.module.ts
    ajuste para rodar com .env

--- TrabalhoP1/backend/src/database/database.module.ts
+++ TrabalhoP1/backend/src/database/database.module.ts
@@ -1,17 +1,22 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '1234',
-      database: 'barbearia_db',
-      autoLoadModels: true,
-      synchronize: true, 
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: Number(configService.get<number>('DB_PORT')),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASS'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadModels: true,
+        synchronize: true,
+      }),
     }),
   ],
 })
