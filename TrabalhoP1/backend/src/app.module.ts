5f90fe5dd42fa7cba16b6012dc43a6630e6eeed3
Thiago Allves <101152758+thiallves@users.noreply.github.com>
2026-04-24 17:15:32 -0300

    Update app.module.ts
    ajuste para rodar com .env

--- TrabalhoP1/backend/src/app.module.ts
+++ TrabalhoP1/backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
@@ -7,7 +8,15 @@ import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [DatabaseModule, BarbershopsModule, AuthModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    BarbershopsModule,
+    AuthModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
